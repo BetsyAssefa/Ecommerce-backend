b6b5e8c6cc33d132b3bf42a2cc9fc69e45810eb2
betsy <bitsietassefa@icloud.com>
2024-11-24 03:48:23 -0600

    Initial commit for E-commerce Backend

---
+++ models/Tag.js
@@ -0,0 +1,30 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection.js");
+
+class Tag extends Model {}
+
+Tag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+
+    tag_name: {
+      type: DataTypes.STRING,
+    },
+    // define columns
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "tag",
+  }
+);
+
+module.exports = Tag;
